48415f53bc0197e3c37db55fba3193ab39538664
Saint-Jean Germila <germila@noukod.com>
2018-08-31 20:13:43 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,11 @@
-def valid_move?(board,index)
-    if move == valid
-      true
-    else
+def position_taken?(board,index)
+    if board[index]= " " || board[index]= "" || board[index]= nil
       false
+    else
+   true
     end
     end
+    
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
